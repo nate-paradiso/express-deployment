0ea1cdb1705860be1ce3c92015e2e959945cb8cf
Nate Paradiso <ipfcolo05@gmail.com>
2023-11-16 09:11:19 -0800

    message new page

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5050;
 app.get("/", (req, res) => {
 	res.send(`<h1>Welcome to my Nate's Express App</h1>`);
 });
+app.get("/nate", (req, res) => {
+	res.send(`<h1>Welcome to Nate's pages</h1>`);
+});
 
 app.listen(PORT, () => {
 	console.log(`app running on port ${PORT}`);
